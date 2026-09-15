24eafc55952b815a72ec8f91aa70ccd659edf00e
Hanaserbedija <hserbedija@gmail.com>
2026-09-15 17:16:00 +0200

    Add files via upload

---
+++ putna agencija/putna agencija.js
@@ -0,0 +1,12 @@
+function pozdrav(){
+    let imePrezime = prompt("upiši svoje ime i prezime:");
+    alert("Hej." + imePrezime + "! Dobrodošli u našu agenciju.");
+}
+
+function zamijeniBoju(){
+    document.body.style.backgroundColor="white";
+}
+
+function pokaziPoruku(){
+    alert("Dobro si kliknuo");
+}
